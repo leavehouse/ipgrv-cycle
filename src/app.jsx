99d87bbc19b46a7574db7fe2ca8392daaed3dea3
leavehouse <heavelouse@gmail.com>
2017-11-16 14:44:45 -0600

    Add basic filetree navigation

--- src/app.jsx
+++ src/app.jsx
@@ -1,35 +1,69 @@
 import xs from 'xstream'
+import dropRepeats from 'xstream/extra/dropRepeats';
 
 export function App (sources) {
-  function newFile(name) {
-    return {'type': 'file', 'name': name}
-  }
-  function newFolder(name) {
-    return {'type': 'folder', 'name': name}
-  }
-  const state$ = xs.of([newFile('foo'),
-                        newFolder('bar'),
-                        newFile('baz')]);
+  const history$ = sources.DOM.select('span.nav-link').events('click')
+    .map(ev => ev.target.dataset.filename || "/")
+    .compose(dropRepeats());
+
+  const subtree = {'hi_there': {'type': 'file'},
+                   'face_here': {'type': 'file'}};
+  const filetree = {'foo': {'type': 'file'},
+                    'bar': {'type': 'folder', 'children': subtree},
+                    'baz': {'type': 'file'}};
+  const state$ = xs.of({'filetree': filetree});
   return {
-    DOM: view(state$)
+    DOM: view(state$, sources.history),
+    history: history$,
   }
 }
 
-function view(state$) {
-  return state$.map(files => {
-    const filesList = files.map(f => {
-      const iconClass = f.type === 'file' ? 'fa-file-text-o' : 'fa-folder';
+function traverseToSubtree(path, filetree) {
+  if (path === '/') {
+    return filetree;
+  }
+  const pathSegments = path.slice(1).split('/');
+  let subtree = filetree;
+  for (const pathSegment of pathSegments) {
+    const subEntry = subtree[pathSegment];
+    if (subEntry.type === 'folder') {
+      subtree = subEntry.children;
+    } else {
+      throw new Error("Viewing blobs is currently unimplemented");
+    }
+  }
+  return subtree;
+}
+
+function view(state$, history$) {
+  return history$.map(history => {
+    const {pathname} = history;
+    return state$.map(state => {
+      const subtree = traverseToSubtree(pathname, state.filetree);
+      let subtreeList = [];
+      for (const [name, props] of Object.entries(subtree)) {
+        subtreeList.push({'name': name, 'type': props.type});
+      }
+      const filesList = subtreeList.map(f => {
+        const iconClass = f.type === 'file' ? 'fa-file-text-o' : 'fa-folder';
+        return (
+          <tr className="navigation">
+            <td><i className={'fa '+iconClass} aria-hidden="true"></i></td>
+            <td><span className="nav-link" data-filename={f.name}>{f.name}</span></td>
+          </tr>
+        );
+      });
       return (
-        <tr>
-          <td><i className={'fa '+iconClass} aria-hidden="true"></i></td>
-          <td>{f.name}</td>
-        </tr>
+        <table className="filetree">
+          <tbody>
+            { pathname !== '/' &&
+              <tr className="navigation"><td colspan="2"><span className="nav-link">..</span></td></tr> }
+          </tbody>
+          <tbody>
+            {filesList}
+          </tbody>
+        </table>
       );
     });
-    return (
-      <table>
-        {filesList}
-      </table>
-    );
-  });
+  }).flatten();
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import {run} from '@cycle/run'
 import {makeDOMDriver} from '@cycle/dom'
+import {makeHashHistoryDriver} from '@cycle/history';
 import {App} from './app'
 
 const main = App
 
 const drivers = {
-  DOM: makeDOMDriver('#root')
+  DOM: makeDOMDriver('#root'),
+  history: makeHashHistoryDriver(),
 }
 
 run(main, drivers)
